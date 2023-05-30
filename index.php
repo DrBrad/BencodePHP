ea4ca7070ea4be70af929ea7263cde83fb78f2b7
Brad <brad@bradeagle.com>
2023-05-30 14:17:19 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    //$buf = file_get_contents('BLANK.torrent');
+    $buf = 'd4:dictd3:1234:test3:4565:thinge4:listl11:list-item-111:list-item-2e6:numberi123456e6:string5:valuee';
+
+    $bencode = new Bencode($buf);
+    $result = $bencode->decode();
+
+    print_r(json_encode($bencode, JSON_PRETTY_PRINT));
+?>
